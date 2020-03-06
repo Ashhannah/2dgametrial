9563ddec577f09a401cc9131251e60bdcf300d65
ash <339605@colum.edu>
2020-03-05 22:16:23 -0600

    enemy collision
    test

---
+++ Assets/scripts(mine)/damagezone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damagezone : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        rubycontroller controller = other.GetComponent<rubycontroller >();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+
+
+    }
+        
+    void OnColisionEnter2D(Collision2D other)
+    {
+        rubycontroller player = other.gameObject.GetComponent<rubycontroller>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+            }
+    }
+}
